d199242528aee75e6ba4311f524779e7a51d0104
Karl Sixten Bjarnø <karlsixten@gmail.com>
2023-11-21 11:46:17 +0100

    Added CompetitionSwimmer.java

--- src/main/java/domain/CompetitionSwimmer.java
+++ src/main/java/domain/CompetitionSwimmer.java
@@ -7,7 +7,7 @@ public class CompetitionSwimmer extends Member{
     private String category;
     private Coach coach;
 
-    
+
     public CompetitionSwimmer(String fullName, String userID, LocalDate birthDate, String email,
                               int phoneNumber, String address, String gender, Membership membership){
         super(fullName, userID, birthDate, email,
