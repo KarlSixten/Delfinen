da09e12710e8c87c198ad76f3adfe66e12a39477
Karl Sixten Bjarnø <karlsixten@gmail.com>
2023-11-22 11:47:36 +0100

    Added createNewMember in ui

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -25,19 +25,19 @@ public class UserInterface {
                 case 4 -> deleteMember();
                 case 5 -> editMember();
                 case 9 -> exitProgram();
-                default -> System.out.println("Invalid selection! Try agan:\n");
+                default -> System.out.println("Ugyldigt valg! Prøv igen:\n");
             }
         }
     }
 
     private void showMainMenu() {
         System.out.println("""
-                Select the function you'd like to use:
-                1. Create new member.
-                2. Show all members.
-                3. Find a specific member.
-                4. Delete a member.
-                5. Edit a member.
+                Vælg den funktion du vil tilgå:
+                1. Opret nyt medlem.
+                2. Vis alle medlemmer.
+                3. Find et specifikt medlem.
+                4. Slet et medlem.
+                5. Rediger et medlem.
                 """);
     }
 
@@ -48,7 +48,7 @@ public class UserInterface {
         try {
             inputInt = Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            System.out.println("Invalid input! Try again:");
+            System.out.println("Ulgyldigt input! Prøv igen:");
             inputInt = takeIntUserInput();
         }
         return inputInt;
@@ -71,18 +71,94 @@ public class UserInterface {
     }
 
     private void createMember() {
-        //Test
-        controller.createNewUser("Karl Bjarnø",
-                LocalDate.ofEpochDay(19990619),
-                "karlsixten@gmail.com",
-                60626656,
-                "Ungarnsgade 72, 3.tv",
-                "Male",
-                true,
-                false,
-                false,
-                false);
+        System.out.println("Indtast fulde navn:");
+        String fullName = scanner.nextLine();
 
+        //HER KAN MAN KUN VÆRE FØDT I OKTOBER ELLER EFTER
+        System.out.println("Indtast fødselsdato (ååååmmdd):");
+        String birthdateString = scanner.nextLine();
+        LocalDate birthDate = LocalDate.of(Integer.parseInt(birthdateString.substring(0, 3)), Integer.parseInt(birthdateString.substring(4, 5)), Integer.parseInt(birthdateString.substring(6, 7)));
+
+        System.out.println("Indtast e-mail:");
+        String email = scanner.nextLine();
+
+        System.out.println("Indtast tlf. nr.:");
+        int phoneNumber = takeIntUserInput();
+
+        System.out.println("Indtast addresse:");
+        String address = scanner.nextLine();
+
+        System.out.println("""
+                Vælg køn:
+                1. Kvinde
+                2. Mand""");
+        String gender = "Male";
+        int userSelection = takeIntUserInput();
+
+        while (userSelection !=1 && userSelection !=2) {
+            System.out.println("Ugyldigt valg! Prøv igen:");
+            userSelection = takeIntUserInput();
+        }
+
+        if (userSelection == 1) {
+            gender = "Woman";
+        }
+
+        //This has been set to true since it doesn't make sense to create an inactive user
+        boolean isActive = true;
+
+        System.out.println("""
+                Er medlemmet senior?
+                1. Ja
+                2. Nej
+                """);
+        userSelection = takeIntUserInput();
+        boolean isSenior = false;
+
+        while (userSelection !=1 && userSelection !=2) {
+            System.out.println("Ugyldigt valg! Prøv igen:");
+            userSelection = takeIntUserInput();
+        }
+
+        if (userSelection == 1) {
+            isSenior = true;
+        }
+
+        System.out.println("""
+                Er medlemmet konkurrencesvømmer?
+                1. Ja
+                2. Nej
+                """);
+        userSelection = takeIntUserInput();
+        boolean isCompetitive = false;
+
+        while (userSelection !=1 && userSelection !=2) {
+            System.out.println("Ugyldigt valg! Prøv igen:");
+            userSelection = takeIntUserInput();
+        }
+
+        if (userSelection == 1) {
+            isCompetitive = true;
+        }
+
+        System.out.println("""
+                Er medlemmet træner?
+                1. Ja
+                2. Nej
+                """);
+        userSelection = takeIntUserInput();
+        boolean isCoach = false;
+
+        while (userSelection !=1 && userSelection !=2) {
+            System.out.println("Ugyldigt valg! Prøv igen:");
+            userSelection = takeIntUserInput();
+        }
+
+        if (userSelection == 1) {
+            isCoach = true;
+        }
+
+        controller.createNewUser(fullName, birthDate, email, phoneNumber, address, gender, isActive, isSenior, isCompetitive, isCoach);
     }
 
     private void exitProgram() {
