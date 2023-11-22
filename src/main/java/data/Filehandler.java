5af6e34ca20b0f2569b51de7db25677b9d189084
AndersBondeLudvigsen <anlu0001@stud.kea.dk>
2023-11-22 10:09:35 +0100

    Filehandler tilpasset

--- src/main/java/data/Filehandler.java
+++ src/main/java/data/Filehandler.java
@@ -29,30 +29,6 @@ public class Filehandler {
         }
     }
 
-
-
-
-
-  /*  public ArrayList<Member> loadData() throws IOException {
-        ArrayList<Member> dataArraylist = new ArrayList<>();
-
-        Scanner scanner = new Scanner(new File("Datasheet.csv"));
-
-        while (scanner.hasNextLine()){
-            String line = scanner.nextLine();
-            String[] parts = line.split(";");
-            dataArraylist.add(new Member(parts[0],
-                    parts[1],
-                    LocalDate.parse(parts[2]),
-                    parts[3],
-                    Integer.parseInt(parts[4]),
-                    parts[5],
-                    parts[6],
-                    parts[7]));
-        }
-        return dataArraylist;
-
-    }*/
     public ArrayList<Member> loadData() throws  IOException{
             ArrayList<Member> memberArrayList = new ArrayList<>();
             Scanner scanner = new Scanner(new File("Datasheet.csv"));
@@ -70,52 +46,11 @@ public class Filehandler {
                         values[5],
                         values[6],
                         Membership.parse(values[7])));
-
-
-               /*         String fullName = values [0],
-             String userID = values [1],
-             LocalDate birthDate = LocalDate.parse(values[2]),
-             String email = values [3],
-             int phoneNumber = Integer.parseInt(values[4]),
-             String address = values [5],
-             String gender = values [6];))*/
-
-/*
-                boolean isActive = Boolean.parseBoolean(values[7]);
-                boolean isSenior = Boolean.parseBoolean(values[8]);
-                boolean isCompetitive = Boolean.parseBoolean(values[9]);
-                boolean isCoach = Boolean.parseBoolean(values [10]);*/
-
-               /* Membership membership = new Membership( isActive,  isSenior,  isCompetitive,  isCoach);
-
-                Member member = new Member(fullName, userID,birthDate,email,phoneNumber,address,gender,membership);
-                database.add(member);*/
             }
 
         }return memberArrayList;
     }
 
 
-/*    public void loadSuperheros(ArrayList<Superhero> database) {
-        Scanner scanner;
-        try {
-            scanner = new Scanner(superheroDatabase);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] values = line.split(";");
-                if (values.length == 6) {
-                    String name = values[0];
-                    String realName = values[1];
-                    String superPower = values[2];
-                    int yearCreated = Integer.parseInt(values[3]);
-                    boolean isHuman = Boolean.parseBoolean(values[4]);
-                    int strength = Integer.parseInt(values[5]);
 
-                    Superhero superhero = new Superhero(name, realName, superPower, yearCreated, isHuman, strength);
-                    database.add(superhero);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
 }
