6def35be801464ce932dc03be37ae9c022801953
AndersBondeLudvigsen <anlu0001@stud.kea.dk>
2023-12-06 10:21:41 +0100

    færdig slut

--- src/main/java/domain/CompetitionSwimmer.java
+++ src/main/java/domain/CompetitionSwimmer.java
@@ -2,8 +2,7 @@ package domain;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class CompetitionSwimmer extends Member {
     private Coach coach;
